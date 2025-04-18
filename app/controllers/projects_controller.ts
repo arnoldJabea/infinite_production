2ae5203a3daf2f1a5c1caa33518cd40c5fc343b8
Arnold Jabea <aroljabea71@gmail.com>
2025-04-18 09:33:28 +0200

    clean unused arguments in ProjectsController

--- app/controllers/projects_controller.ts
+++ app/controllers/projects_controller.ts
@@ -1,28 +1,9 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class ProjectsController {
-  /**
-   * Display a list of resource
-   */
-  async index({}: HttpContext) {}
-
-  /**
-   * Handle form submission for the create action
-   */
-  async store({ request }: HttpContext) {}
-
-  /**
-   * Show individual record
-   */
-  async show({ params }: HttpContext) {}
-
-  /**
-   * Handle form submission for the edit action
-   */
-  async update({ params, request }: HttpContext) {}
-
-  /**
-   * Delete record
-   */
-  async destroy({ params }: HttpContext) {}
+  async index(_ctx: HttpContext) {}
+  async store({ request: _request }: HttpContext) {}
+  async show(_ctx: HttpContext) {}
+  async update(_ctx: HttpContext) {}
+  async destroy(_ctx: HttpContext) {}
 }
